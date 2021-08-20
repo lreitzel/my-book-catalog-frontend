62c03f67f93f5773fc1087dcb5b36be917145108
Lydia <lreitzel@gmail.com>
2021-08-20 18:12:06 -0400

    added renderBook and renderBookInfo methods

--- book.js
+++ book.js
@@ -5,4 +5,23 @@ class Book {
         this.read = read;
         this.genre_id = genre_id;
     }
+
+    renderBook(){
+        const bookLi = document.createElement('li');
+        bookLi.innerText = `${this.title}`;
+        bookLi.addEventListener('click', () => {
+            const bookInfo = this.renderBookInfo();
+            bookLi.appendChild(bookInfo);
+        })
+        return bookLi;
+    }
+
+    renderBookInfo(){
+        const bookDiv = document.createElement('div');
+        bookDiv.innerHTML = `
+            <h3>Author: ${this.author}</h3>
+            <h3>Read: ${this.read}</h3>
+        `
+        return bookDiv;
+    }
 }
