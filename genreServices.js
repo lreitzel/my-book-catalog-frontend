aba02fef3261fc664d80ae9291cfdb866f4e5f61
Lydia <lreitzel@gmail.com>
2021-08-20 00:49:23 -0400

    added initial genre fetch

--- genreServices.js
+++ genreServices.js
@@ -1,3 +1,29 @@
 class GenreServices {
+
+    constructor(baseUrl){
+        this.baseUrl = baseUrl;
+    };
+
+    getGenres(){
+        fetch(this.baseUrl)
+        .then(resp => resp.json())
+        .then(genres => {
+            genres.forEach(genre => {
+                const genreList = document.getElementById('book-container');
+                const genreName = document.createElement('div');
+                genreName.innerHTML = ''
+                genreName.innerHTML += `<h3>${genre.name}</h3>`
+                genreList.appendChild(genreName)
+            });
+        });
+    };
+
+    // renderGenre(){
+    //     const genreList = document.getElementById('book-container');
+    //     const genreName = document.createElement('div');
+    //     genreName.innerHTML = ''
+    //     genreName.innerHTML += `<h3>${genre.name}</h3>`
+    //     genreList.appendChild(genreName)
+    // }
     
 }
