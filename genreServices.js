ccedb0a6e4db699a07781c1fbb530d8a67b69a2e
Lydia <lreitzel@gmail.com>
2021-08-20 18:13:02 -0400

    cleaned up genre fetch request

--- genreServices.js
+++ genreServices.js
@@ -9,21 +9,10 @@ class GenreServices {
         .then(resp => resp.json())
         .then(genres => {
             genres.forEach(genre => {
-                const genreList = document.getElementById('book-container');
-                const genreName = document.createElement('div');
-                genreName.innerHTML = ''
-                genreName.innerHTML += `<h3>${genre.name}</h3>`
-                genreList.appendChild(genreName)
+                const gen = new Genre(genre);
+                gen.renderGenre();
             });
         });
     };
-
-    // renderGenre(){
-    //     const genreList = document.getElementById('book-container');
-    //     const genreName = document.createElement('div');
-    //     genreName.innerHTML = ''
-    //     genreName.innerHTML += `<h3>${genre.name}</h3>`
-    //     genreList.appendChild(genreName)
-    // }
     
 }
