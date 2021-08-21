e7f552858531cb763ddcf2ddef97566e8178147c
Lydia <lreitzel@gmail.com>
2021-08-21 14:24:45 -0400

    added createBook method

--- bookServices.js
+++ bookServices.js
@@ -2,5 +2,24 @@ class BookServices {
     constructor(baseURL) {
         this.baseURL = baseURL;
     };
+
+    createBook(book){
+        const config = {
+            method:'Post',
+            header: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(book)
+        };
+
+        fetch(this.baseURL, config)
+        .then(resp => resp.json())
+        .then(data => {
+            const newBook = new Book(data)
+            const genreList = document.getElementById(`list-${newBook.genre_id}`)
+            genreList.appendChild(newBook.renderBook()) 
+        });
+    };
     
 }
